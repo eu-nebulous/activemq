0e09a546c60b50aaaa0910fafc26b0051b0aae3a
robert.sanfeliu <robert.sanfeliu@eurecat.org>
2026-01-13 11:11:23 +0100

    add users to the broker

--- docker-control-plane/entrypoint.sh
+++ docker-control-plane/entrypoint.sh
@@ -7,9 +7,10 @@ if [ -z "$ACTIVEMQ_ADMIN_PASSWORD" ]; then
   exit 1
 fi
 
-# Replace placeholder in bootstrap.xml
-echo "Replacing \${ACTIVEMQ_ADMIN_PASSWORD} with env var in bootstrap.xml..."
-sed -i "s|\${ACTIVEMQ_ADMIN_PASSWORD}|$ACTIVEMQ_ADMIN_PASSWORD|g" /var/lib/artemis-instance/etc-override/bootstrap.xml
-
+# Replace placeholder in artemis-users.properties
+echo "Replacing \${ACTIVEMQ_ADMIN_PASSWORD}  with env var in artemis-users.properties..."
+sed -i "s|\${ACTIVEMQ_ADMIN_PASSWORD}|$ACTIVEMQ_ADMIN_PASSWORD|g" /var/lib/artemis-instance/etc-override/artemis-users.properties
 # Proceed with normal activemq startup
+echo "Replacing \${ACTIVEMQ_APP_CLIENT_PASSWORD} with env var in artemis-users.properties..."
+sed -i "s|\${ACTIVEMQ_APP_CLIENT_PASSWORD}|$ACTIVEMQ_APP_CLIENT_PASSWORD|g" /var/lib/artemis-instance/etc-override/artemis-users.properties
 exec /docker-run.sh run "$@"
